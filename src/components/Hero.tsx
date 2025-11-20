dbc784a12812f823a896879fb66f6d9dba5a939d
DoctorTone <tony@drt-software.com>
2025-11-20 18:09:06 +0000

    Update hero section with chip

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,18 +1,25 @@
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import Chip from "@mui/material/Chip";
+import BoltIcon from "@mui/icons-material/Bolt";
 
 const Hero = () => {
   return (
     <Container sx={{ mt: 20 }} maxWidth="xl">
-      <Grid
-        container
-        spacing={6}
-        sx={{ display: "flex", alignItems: "center" }}
-      >
+      <Chip
+        icon={<BoltIcon />}
+        label="Home"
+        variant="outlined"
+        color="warning"
+      />
+      <Grid container spacing={6} sx={{ display: "flex" }}>
         <Grid size={6}>
           <Typography variant="h3">
             Complex data made <span style={{ color: "orange" }}>simple</span>.
+          </Typography>
+          <Typography variant="h6">
+            We can show your data in a whole new dimension.
           </Typography>
         </Grid>
         <Grid size={6} className="bordered">
