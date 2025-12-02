e5b6356637bb253dd217e4573a777d92a4408767
DoctorTone <tony@drt-software.com>
2025-12-02 15:37:53 +0000

    Added components to dataviz page

--- src/pages/DataViz.tsx
+++ src/pages/DataViz.tsx
@@ -1,9 +1,15 @@
 import NavBarDataViz from "../components/NavBarDataViz";
+import Portfolio from "../components/Portfolio";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 const DataViz = () => {
   return (
     <>
       <NavBarDataViz />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </>
   );
 };
