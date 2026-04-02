f18786075f086e40a40605910eb19f4d48ee493f
DoctorTone <tony@drt-software.com>
2026-04-03 00:25:16 +0100

    Video and project details added for case study

--- src/components/CaseHeatIsland.tsx
+++ src/components/CaseHeatIsland.tsx
@@ -3,6 +3,9 @@ import Chip from "@mui/material/Chip";
 import BoltIcon from "@mui/icons-material/Bolt";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import LeaderboardIcon from "@mui/icons-material/Leaderboard";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
 const CaseHeatIsland = () => {
   return (
@@ -14,11 +17,11 @@ const CaseHeatIsland = () => {
         color="warning"
         sx={{ mb: 2, color: "orange", borderColor: "orange" }}
       />
-      <Grid container>
+      <Grid container sx={{ mb: 4 }}>
         <Grid size={{ md: 2 }}></Grid>
         <Grid size={{ xs: 12, md: 8 }}>
-          <Typography variant="h3" sx={{ mt: 1, mb: 1, textAlign: "center" }}>
-            Heat Islands
+          <Typography variant="h3" sx={{ mb: 3, textAlign: "center" }}>
+            London Urban Heat Islands
           </Typography>
           <video
             muted
@@ -31,6 +34,48 @@ const CaseHeatIsland = () => {
           </video>
         </Grid>
       </Grid>
+      <Box sx={{ mb: 6 }}>
+        <Typography variant="h4" sx={{ mt: 3, mb: 3 }}>
+          The <span style={{ color: "orange" }}>Project</span>
+        </Typography>
+        <Typography variant="h6" sx={{ color: "grey", mb: 3 }}>
+          This is an interactive 3D visualisation of London's urban heat
+          patterns using satellite thermal imaging data and more than 27,000
+          building footprints. It reveals the spatial relationships that inform
+          climate adaptation and urban planning strategies.
+        </Typography>
+        <Typography variant="h6">
+          <LeaderboardIcon
+            color="warning"
+            sx={{ verticalAlign: "sub", mr: 2 }}
+          />
+          27,723 buildings visualised.
+        </Typography>
+        <Typography variant="h6">
+          <LeaderboardIcon
+            color="warning"
+            sx={{ verticalAlign: "sub", mr: 2 }}
+          />
+          Temperatrure range of 27.5C to 44.6C.
+        </Typography>
+        <Typography variant="h6" sx={{ mb: 3 }}>
+          <LeaderboardIcon
+            color="warning"
+            sx={{ verticalAlign: "sub", mr: 2 }}
+          />
+          Optimal performnce of 60 frames per second on desktop, tablet and
+          mobile.
+        </Typography>
+        <Button
+          variant="contained"
+          color="warning"
+          href="https://www.drt-software.com/HeatIsland/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          View Demo
+        </Button>
+      </Box>
     </Container>
   );
 };
