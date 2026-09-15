e37cefd0a2fb55b93a8741b2b3ee546a6e01d59c
DoctorTone <tony@drt-software.com>
2026-09-15 14:32:09 +0100

    Added Springwell solar farm video

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -113,7 +113,7 @@ const Portfolio = () => {
         </Grid>
         <Grid size={{ xs: 12, md: 4 }} sx={{ pr: 2 }}>
           <a
-            href="https://drt-software.com/PointCloud/"
+            href="https://drt-software.com/SolarViz/"
             target="_blank"
             rel="noreferrer noopener"
           >
@@ -124,17 +124,17 @@ const Portfolio = () => {
               autoPlay
               className="borderedLight w-100"
             >
-              <source src="./videos/PointCloud.mp4" type="video/mp4" />
+              <source src="./videos/Springwell.mp4" type="video/mp4" />
             </video>
           </a>
           <Box sx={{ textAlign: "center" }}>
             <Typography variant="h6" sx={{ mt: 1, mb: 1 }}>
-              City point cloud
+              Solar farm visualisation
             </Typography>
             <Button
               variant="contained"
               color="warning"
-              href="https://drt-software.com/PointCloud/"
+              href="https://drt-software.com/SolarViz/"
               target="_blank"
               rel="noreferrer noopener"
             >
